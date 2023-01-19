5fc1ffbcf5d15e809343ca9ae8c604f8ebe1290c
Kaichen <276126767@qq.com>
2023-01-19 01:24:11 +0100

    feat: 增加系统托盘

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,11 @@
     windows_subsystem = "windows"
 )]
 
+mod tray;
 fn main() {
     tauri::Builder::default()
+        .system_tray(tray::main_menu())
+        .on_system_tray_event(tray::handler)
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
---
+++ src-tauri/src/tray.rs
@@ -0,0 +1,45 @@
+use tauri::{AppHandle, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+
+// 加载菜单
+pub fn main_menu() -> SystemTray {
+    let tray_menu = SystemTrayMenu::new().add_item(CustomMenuItem::new("quit".to_string(), "Quit")); // 退出
+
+    SystemTray::new().with_menu(tray_menu)
+}
+
+// 菜单事件
+pub fn handler(app: &AppHandle, event: SystemTrayEvent) {
+    let window = app.get_window("main").unwrap();
+
+    // 匹配点击事件
+    match event {
+        // 左键点击
+        SystemTrayEvent::LeftClick {
+            position: _,
+            size: _,
+            ..
+        } => {
+            window.show().unwrap();
+            window.set_focus().unwrap();
+        }
+        // 右键点击
+        SystemTrayEvent::RightClick {
+            position: _,
+            size: _,
+            ..
+        } => {
+            println!("system tray received a right click");
+        }
+        // 根据菜单 id 进行事件匹配
+        SystemTrayEvent::MenuItemClick { id, .. } => {
+            let _item_handle = app.tray_handle().get_item(&id);
+            match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                _ => {}
+            }
+        }
+        _ => {}
+    }
+}
